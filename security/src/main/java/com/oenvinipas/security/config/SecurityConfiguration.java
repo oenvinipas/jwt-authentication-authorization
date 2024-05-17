fa93b24cfd62112e4a5c4b46c553dcdb4f2fc115
Santiago <oenvinipas2@gmail.com>
2024-05-17 15:46:02 -0400

    security configuration

--- security/src/main/java/com/oenvinipas/security/config/SecurityConfiguration.java
+++ security/src/main/java/com/oenvinipas/security/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.oenvinipas.security.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
